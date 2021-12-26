987baa2bdb635e85a155ea9f14b2febadfb27b49
rewine <lhongxu@outlook.com>
2021-12-26 18:59:07 +0800

    Dump site-lisp

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elfeed-feeds '("https://88250.b3log.org/rss.xml")))
+ '(elfeed-feeds '("https://88250.b3log.org/rss.xml"))
+ '(package-selected-packages '(dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
