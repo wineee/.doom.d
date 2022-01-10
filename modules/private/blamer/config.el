6b0a061cdc0ae1b0746d13e9a83d0e51f520ac24
rewine <lhongxu@outlook.com>
2022-01-10 22:12:06 +0800

    Add blamer.el modules

---
+++ modules/private/blamer/config.el
@@ -0,0 +1,14 @@
+;;; private/blamer/config.el -*- lexical-binding: t; -*-
+
+(use-package blamer
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 70)
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                    :background nil
+                    :height 140
+                    :italic t)))
+  :config
+  (global-blamer-mode 1))
---
+++ modules/private/blamer/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; private/blamer/packages.el
+
+(package! blamer)
+;; or
+(package! blamer :recipe (:host github :repo "artawower/blamer.el"))
