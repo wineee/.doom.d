b3a96f4153b166cb6f1f9807874f51cd662e7fe5
rewine <lhongxu@outlook.com>
2022-01-12 16:05:52 +0800

    eaf evil mode

--- tool/init-eaf.el
+++ tool/init-eaf.el
@@ -3,7 +3,7 @@
   :config
   ;;(use-package eaf-video-player)
   (require 'eaf-file-manager)
-  (require 'eaf-browser)
+  (require 'eaf-browser nil t)
   (require 'eaf-netease-cloud-music)
   (require 'eaf-music-player)
   (require 'eaf-system-monitor)
@@ -15,6 +15,7 @@
   ;;(require 'eaf-org-previewer)
   (require 'eaf-video-player)
   (require 'eaf-demo)
+  (require 'eaf-evil)
 
   (defalias 'browse-web #'eaf-open-browser)
   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
@@ -22,10 +23,29 @@
   (eaf-bind-key nil "M-q" eaf-browser-keybinding) ;; unbind, see more in the Wiki
 
   :custom
-  ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
+  (browse-url-browser-function #'eaf-open-browser) ;; Make EAF Browser my default browser
   (eaf-browser-continue-where-left-off t)
+  (eaf-start-python-process-when-require t)
+  (eaf-browser-default-zoom 1.25)
+  (eaf-browser-dark-mode nil)
   (eaf-browser-enable-adblocker t)
-  (browse-url-browser-function 'eaf-open-browser)
+  (eaf-browser-enable-autofill t)
+  (eaf-file-manager-show-preview t)
+  (eaf-pdf-dark-mode "ignore")
 )
+
+
+(define-key key-translation-map (kbd "SPC")
+    (lambda (prompt)
+      (if (derived-mode-p 'eaf-mode)
+          (pcase eaf--buffer-app-name
+            ("browser" (if  (string= (eaf-call-sync "call_function" eaf--buffer-id "is_focus") "True")
+                           (kbd "SPC")
+                         (kbd eaf-evil-leader-key)))
+            ("pdf-viewer" (kbd eaf-evil-leader-key))
+            ("image-viewer" (kbd eaf-evil-leader-key))
+            (_  (kbd "SPC")))
+        (kbd "SPC"))))
+
 
 (provide 'init-eaf)
