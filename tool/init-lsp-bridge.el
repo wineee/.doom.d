764fe48589ff77332c8a1ab18e8af37f9c549826
rewine <lhongxu@outlook.com>
2023-06-11 20:08:28 +0800

    add key map for lsp-bridge

--- tool/init-lsp-bridge.el
+++ tool/init-lsp-bridge.el
@@ -9,6 +9,18 @@
 (require 'lsp-bridge)
 (global-lsp-bridge-mode)
 
+(map! :map acm-mode-map
+      [tab]           #'acm-select-next
+      [backtab]       #'acm-select-prev)
+
+(map! :map doom-leader-code-map
+      :desc "LSP Rename"
+      "r"             #'lsp-bridge-rename
+      :desc "LSP Find declaration"
+      "j"             #'lsp-bridge-find-def)
+
+(setq lsp-bridge-enable-log 1)
+
 (provide 'init-lsp-bridge)
 
 ;;; init-lsp-bridge.el ends here
